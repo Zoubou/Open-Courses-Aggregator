4c513f5b090b02b27cd52ce9b9fc9ac10884e010
Konstantina Magalou <konnamagalou@gmail.com>
2026-01-24 13:00:09 +0200

    .

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -10,8 +10,4 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
     
-    // Τρέξε τον harvester αυτόματα μετά από λίγο καιρό
-    setTimeout(() => {
-        autoRunHarvester();
-    }, 2000); // Περίμενε 2 δευτερόλεπτα ώστε η σύνδεση με τη ΒΔ να γίνει
 });
