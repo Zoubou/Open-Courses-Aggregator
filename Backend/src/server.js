c2c5d6499a3008071936f2431df0dec5748af624
zoubou <your.jzoub03@gmail.com>
2026-01-24 12:35:02 +0200

    deleted autoRunHarvester

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -6,30 +6,6 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const PORT = 3000;
 
-// Συνάρτηση για αυτόματο τρέξιμο του harvester
-function autoRunHarvester() {
-    const sources = ['kaggle', 'kaggle2'];
-    
-    console.log("Starting automatic harvester sync...");
-    
-    sources.forEach((source) => {
-        const harvesterDir = path.resolve(__dirname, '../../harvester');
-        const harvesterFile = path.resolve(harvesterDir, 'index.js');
-        
-        console.log(`Triggering sync for source: ${source}`);
-        exec(`node "${harvesterFile}" --source=${source}`, { cwd: harvesterDir }, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`[${source}] EXEC ERROR:`, error.message);
-            }
-            if (stderr) {
-                console.error(`[${source}] STDERR:`, stderr);
-            }
-            if (stdout) {
-                console.log(`[${source}] STDOUT:`, stdout);
-            }
-        });
-    });
-}
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
